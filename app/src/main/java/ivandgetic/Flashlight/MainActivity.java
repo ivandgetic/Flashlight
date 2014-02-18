7074401b4594095c57be4dd159abd3afb21c8e73
ivandgetic <ivandgetic@gmail.com>
2014-02-19 00:57:00 +0800

    Create
    Useful in the evening

---
+++ app/src/main/java/ivandgetic/Flashlight/MainActivity.java
@@ -0,0 +1,75 @@
+package ivandgetic.Flashlight;
+
+import android.app.Activity;
+import android.app.ActionBar;
+import android.os.Bundle;
+import android.view.View;
+import android.hardware.Camera;
+import android.os.PowerManager;
+import android.view.WindowManager;
+import android.widget.SeekBar;
+
+public class MainActivity extends Activity {
+    PowerManager.WakeLock wakeLock;
+    private boolean isopent = false;
+    private Camera camera;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ActionBar actionBar = getActionBar();
+        actionBar.hide();
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        SeekBar seekbar = (SeekBar) findViewById(R.id.seekbar);
+        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress,
+                                          boolean fromUser) {
+                // TODO Auto-generated method stub
+                if (progress > 0) {
+                    LightnessControl.setScreenBrightness(MainActivity.this,
+                            progress);
+                }
+            }
+        });
+    }
+
+    public void kaiguan(View view) {
+        if (!isopent) {
+            camera = Camera.open();
+            Camera.Parameters params = camera.getParameters();
+            params.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+            camera.setParameters(params);
+            camera.startPreview(); //打开闪光灯
+
+            isopent = true;
+        } else {
+            camera.stopPreview(); // 关掉闪光灯
+            camera.release(); // 关掉照相机
+            isopent = false;
+        }
+    }
+
+    static class LightnessControl {
+        public static void setScreenBrightness(Activity activity, int value) {
+            WindowManager.LayoutParams params = activity.getWindow()
+                    .getAttributes();
+            params.screenBrightness = value * (1.0f / 100.0f);
+            activity.getWindow().setAttributes(params);
+        }
+    }
+
+}
