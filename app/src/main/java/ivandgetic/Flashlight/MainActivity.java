fcc4ab75968377033f52e30dfc9f546ba4a991ca
ivandgetic <ivandgetic@gmail.com>
2014-05-13 01:10:18 +0800

    Update Gradle
    Update gradle version.

--- app/src/main/java/ivandgetic/Flashlight/MainActivity.java
+++ app/src/main/java/ivandgetic/Flashlight/MainActivity.java
@@ -3,6 +3,7 @@ package ivandgetic.Flashlight;
 import android.app.Activity;
 import android.app.ActionBar;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.hardware.Camera;
 import android.os.PowerManager;
@@ -10,7 +11,6 @@ import android.view.WindowManager;
 import android.widget.SeekBar;
 
 public class MainActivity extends Activity {
-    PowerManager.WakeLock wakeLock;
     private boolean isopent = false;
     private Camera camera;
 
@@ -18,13 +18,9 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ActionBar actionBar = getActionBar();
-        actionBar.hide();
-
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-
         SeekBar seekbar = (SeekBar) findViewById(R.id.seekbar);
         seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
@@ -36,12 +32,9 @@ public class MainActivity extends Activity {
             }
 
             @Override
-            public void onProgressChanged(SeekBar seekBar, int progress,
-                                          boolean fromUser) {
-                // TODO Auto-generated method stub
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 if (progress > 0) {
-                    LightnessControl.setScreenBrightness(MainActivity.this,
-                            progress);
+                    setScreenBrightness(MainActivity.this, progress);
                 }
             }
         });
@@ -53,23 +46,19 @@ public class MainActivity extends Activity {
             Camera.Parameters params = camera.getParameters();
             params.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
             camera.setParameters(params);
-            camera.startPreview(); //打开闪光灯
-
+            camera.startPreview();
             isopent = true;
         } else {
-            camera.stopPreview(); // 关掉闪光灯
-            camera.release(); // 关掉照相机
+            camera.stopPreview();
+            camera.release();
             isopent = false;
         }
     }
 
-    static class LightnessControl {
-        public static void setScreenBrightness(Activity activity, int value) {
-            WindowManager.LayoutParams params = activity.getWindow()
-                    .getAttributes();
-            params.screenBrightness = value * (1.0f / 100.0f);
-            activity.getWindow().setAttributes(params);
-        }
+    public void setScreenBrightness(Activity activity, int value) {
+        WindowManager.LayoutParams params = activity.getWindow().getAttributes();
+        params.screenBrightness = value * (1.0f / 100.0f);
+        activity.getWindow().setAttributes(params);
     }
 
 }
